52d703d5099f0091a7d8645550c140e9edec6039
Dan Nuttall <pixelblend@gmail.com>
2014-08-11 11:17:42 +0100

    Output as JSON

--- main.js
+++ main.js
@@ -1,22 +1,20 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
-var hookContent = "";
+var hookContent = "{}";
 var port = process.env.PORT || 3000;
 
 app.post('/post-recieve', function(req, res){
   hookContent = req.body;
-  res.send(hookContent);
+  res.json(hookContent);
 });
 
 app.get('/', function(req, res) {
-  res.send(hookContent);
+  res.json(hookContent);
 });
-
-var bodyParser = require('body-parser');
 
 app.use(bodyParser.json());
 
 var server = app.listen(port, function() {
   console.log('Listening on port %d', server.address().port);
 });
-
