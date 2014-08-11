49f25dd7a53b5caf4f54976bf373d9124c4b406d
Dan Nuttall <pixelblend@gmail.com>
2014-08-11 11:43:08 +0100

    Juggle

--- main.js
+++ main.js
@@ -6,6 +6,10 @@ var app = express();
 var hookContent = "{}";
 var port = process.env.PORT || 3000;
 
+app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(logfmt.requestLogger());
+
 app.post("/post-recieve", function(req, res){
   hookContent = req.body;
   console.log("post",req.body);
@@ -15,10 +19,6 @@ app.post("/post-recieve", function(req, res){
 app.get("/", function(req, res) {
   res.json(hookContent);
 });
-
-app.use(bodyParser.json());
-app.use(methodOverride());
-app.use(logfmt.requestLogger());
 
 var server = app.listen(port, function() {
   console.log("Listening on port %d", server.address().port);
