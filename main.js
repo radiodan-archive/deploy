90b463800a65a0ad1bec06c1845bd49a5aebb4d6
Dan Nuttall <pixelblend@gmail.com>
2014-08-11 11:25:55 +0100

    Add some dependencies

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+var logfmt = require("logfmt");
 var app = express();
 var hookContent = "{}";
 var port = process.env.PORT || 3000;
 
-app.post('/post-recieve', function(req, res){
+app.post("/post-recieve", function(req, res){
   hookContent = req.body;
   res.json(hookContent);
 });
 
-app.get('/', function(req, res) {
+app.get("/", function(req, res) {
   res.json(hookContent);
 });
 
 app.use(bodyParser.json());
+app.use(methodOverride());
+app.use(logfmt.requestLogger());
 
 var server = app.listen(port, function() {
-  console.log('Listening on port %d', server.address().port);
+  console.log("Listening on port %d", server.address().port);
 });
