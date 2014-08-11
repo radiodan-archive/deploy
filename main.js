ec708b55f9160c9ebf561c8c17a9764bfe5d71b3
Dan Nuttall <pixelblend@gmail.com>
2014-08-11 17:53:40 +0100

    Does things

--- main.js
+++ main.js
@@ -1,25 +1,36 @@
-var express = require("express");
-var bodyParser = require("body-parser");
-var methodOverride = require("method-override");
-var logfmt = require("logfmt");
-var app = express();
-var hookContent = "{}";
-var port = process.env.PORT || 3000;
+var express        = require('express'),
+    bodyParser     = require('body-parser'),
+    methodOverride = require('method-override'),
+    morgan         = require('morgan'),
+    logfmt         = require('logfmt'),
+    Deployment     = require(__dirname + '/lib/deployment'),
+    persistance    = require(__dirname + '/lib/persistance').create();
+
+var app  = express(),
+    port = (process.env.PORT || 3000);
 
 app.use(bodyParser.json());
 app.use(methodOverride());
+app.use(morgan('combined'));
 app.use(logfmt.requestLogger());
+app.use(express.static(__dirname + "/public"));
 
-app.post("/post-recieve", function(req, res){
-  hookContent = req.body;
-  console.log("post",req.body);
-  res.json(hookContent);
+app.post('/post-hook', function(req, res){
+  try {
+    var deploy = Deployment.create(req.body);
+    deploy.deploy();
+
+    res.status(200).end();
+  } catch(err) {
+    console.warn(err);
+    res.status(500).end();
+  }
 });
 
-app.get("/", function(req, res) {
-  res.json(hookContent);
+app.get('/', function(req, res) {
+  res.json(persistance.data);
 });
 
 var server = app.listen(port, function() {
-  console.log("Listening on port %d", server.address().port);
+  console.log('Listening on port %d', server.address().port);
 });
